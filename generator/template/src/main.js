34c68191d97ff0ff86fad9594d5008ef1b982b37
huqi <hq_1980@hotmail.com>
2021-10-23 13:55:51 +0800

    test

--- generator/template/src/main.js
+++ generator/template/src/main.js
@@ -1,7 +1,6 @@
 import {loadComponent} from '@o2oa/web-component';
 import App from "@/App";
 
-//const name = '<%= componentName %>';
 const name = 'name';
 loadComponent(name, App).then((c)=>{
     c.render();
