881da91f4ff03ef9275333bb4733713585f383fd
huqi <hq_1980@hotmail.com>
2021-11-03 12:01:29 +0800

    modify createApp

--- generator/index.js
+++ generator/index.js
@@ -4,7 +4,7 @@ module.exports = (api, option, rootOptions) => {
         api.render('./template');
     api.extendPackage({
         'dependencies': {
-            '@o2oa/web-component': '^1.1.0'
+            '@o2oa/component': '^1.1.0'
         },
         'scripts': {
             'o2-deploy': 'vue-cli-service build',
