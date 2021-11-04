9798ed000a06f8be33d22e979ef59d8cdb7f99f4
huqi <hq_1980@hotmail.com>
2021-11-04 18:33:46 +0800

    modify createApp

--- generator/template/src/main.js
+++ generator/template/src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue';
 import App from "./App.vue";
 import {loadComponent} from '@o2oa/component';
 
-loadComponent('<%= rootOptions.o2componentName %>', (d)=>{
+loadComponent('<%= rootOptions.o2componentName %>', (d, cb)=>{
     createApp(App).mount(d);
+    cb();
 }).then((c)=>{
     c.render();
 });
