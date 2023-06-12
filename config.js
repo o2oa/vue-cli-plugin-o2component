2afd9f2ebee2275ebf7384b4a9d2c13eace0a750
huqi <hq_1980@hotmail.com>
2023-06-12 17:48:57 +0800

    修复兼容webpack5

--- config.js
+++ config.js
@@ -32,13 +32,49 @@ const proxy = {};
     proxy['^/'+path] = Object.assign({}, o);
 });
 
+const setupMiddlewares = (middlewares, devServer) => {
+    devServer.app.get('/x_desktop/res/config/config.json', (req, res) => {
+        const configUrl = new URL(req.url, host);
+        axios.get(configUrl.toString()).then((json)=>{
+            let o2Config = json.data;
+            o2Config.sessionStorageEnable = true;
+            o2Config.applicationServer = {
+                "host": (proxy.appServer && proxy.appServer.host) ? proxy.appServer.host : server.host
+            };
+            o2Config.center = [{
+                "port": server.port,
+                "host": server.host
+            }];
+            o2Config.proxyApplicationEnable = false;
+            o2Config.proxyCenterEnable = false;
+            res.json(o2Config);
+        }).catch(()=>{
+            res.end();
+        });
+    });
+
+    devServer.app.get(`/${componentPath}/lp/*min.*`, (req, res) => {
+        let toUrl =  path.basename(req._parsedUrl.pathname).replace(/min\./, '')
+        toUrl = path.resolve(process.cwd(), 'public', './lp/'+toUrl);
+        fs.readFile(toUrl).then((data)=>{
+            res.setHeader('Content-Type', 'application/javascript; charset=UTF-8');
+            res.send(data);
+        }, ()=>{
+            res.send('');
+        });
+    });
+
+    return middlewares;
+};
+
 module.exports = {
     publicPath:  process.env.NODE_ENV === 'production' ? "../"+componentPath+"/" : "/"+componentPath+"/",
     outputDir: "./dist/"+componentPath,
     assetsDir: "$Main",
     devServer: {
         proxy: proxy,
-        open: true
+        open: true,
+        setupMiddlewares: server.port!==server.httpPort ? setupMiddlewares : null
     },
     configureWebpack: {
         plugins: [
